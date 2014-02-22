4268b6b214c343dce57ceac0943cfd811f6ec8f7
wmak <william.mak@hotmail.ca>
2014-02-22 14:49:08 -0500

    got very base code working

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
 left = cv2.imread("img/left.jpg", 0)
 right = cv2.imread("img/right.jpg", 0)
@@ -17,10 +16,12 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 # Match Descriptors
 matches = bf.match(des1, des2)
+
 #Sort them in order of distance
 matches = sorted(matches, key = lambda x:x.distance)
 
 #draw first 10 matches
-img3 = cv2.drawMatches(left, kp1, right, kp2, matches[:10], flags=2)
+img3 = None
+img3 = cv2.drawMatches(left, kp1, right, kp2, matches[:10], outImg=img3, flags=2)
 
-plt.imshow(img3),plt.show()
+cv2.imwrite( "Test.jpg", img3)
